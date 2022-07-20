c92d343b713a582b1c75b10addee1547af5209bf
EEGUNJOBI VICTOR ADEOLU <101051829+victoradex@users.noreply.github.com>
2022-07-20 01:14:40 +0000

    assignment 1&2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let arr = arrayOfNumbers;
+  let sum = 0;
+  for(let i=0; i< arr.length; i++){
+    sum+= arr[i]
+  }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,13 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let arr = arrayOfNumbers
+  let count = 0;
+  for(let i= 0;i< arr.length;i++){if((arr[i]%2)== 0)++count;
+  }
+    return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
